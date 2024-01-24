a6d72c2703f74991e5d67e936f5fb997b405f935
Danaree <152726140+Danaree@users.noreply.github.com>
2024-01-24 13:02:09 +0300

    Add files via upload

---
+++ danarjava.java
@@ -0,0 +1,18 @@
+import java.swing.*;
+public class mainFrame extends JFrame{
+public void initialize(){
+    setTitle("wellcome");
+    setsize(500,500);
+    setMinimumSize(new Dimension(400,300));
+setDefultCloseOperation(WindowContants.EXIT.-ON-CLOSE);
+setVisible(true);
+
+
+
+}
+	
+		
+
+	
+
+}
